0adcc9fa1b6b1b200a37e38efec4350b3e59a984
arvind singh <arv9135@outlook.com>
2020-08-01 03:09:18 +0530

    Completed.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  isAuthenticated: boolean;
+  constructor(private authService: AuthService) {
+
+  }
+  async ngOnInit() {
+    await this.authService.checkAuth().toPromise();
+    this.authService.isLoggedIn().subscribe((isAuthenticated) => this.isAuthenticated = isAuthenticated)
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie-service'
+import { SharedModule } from './shared/shared.module';
+import { AuthModule, OidcConfigService, LogLevel } from 'angular-auth-oidc-client';
+import { CookieManagerService } from './shared/services/cookie-manager.service';
+
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig({
+      stsServer: 'https://localhost:44300', //Identity server port
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.origin,
+      clientId: 'angular_spa',
+      scope: 'openid profile offline_access',
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      renewTimeBeforeTokenExpiresInSeconds: 10,
+      logLevel: LogLevel.Debug,
+    });
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AuthModule.forRoot({ storage: CookieManagerService }),
+    SharedModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    CookieService,
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
